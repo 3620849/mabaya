858e5ebc877e48334b91c1bbbf4c9f44cd2f9b6d
Dima <3620849@gmail.com>
2020-06-10 13:47:14 +0300

    add servead controller

---
+++ src/main/java/com/mabaya/demo/controllers/ServeAdController.java
@@ -0,0 +1,19 @@
+package com.mabaya.demo.controllers;
+
+import com.mabaya.demo.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ServeAdController {
+    @RequestMapping(value = "/serveAd/{category}", method = RequestMethod.GET)
+    ResponseEntity<Product> serveAd(@PathVariable("category") String category){
+        return new ResponseEntity<>(  HttpStatus.OK);
+    }
+}
