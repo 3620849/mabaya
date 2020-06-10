1a8e756976484a0cb2c7da99a4c300f072c3fbf7
Dima <3620849@gmail.com>
2020-06-10 13:44:59 +0300

    add insert campaign impl at service

---
+++ src/main/java/com/mabaya/demo/repositories/CampaignRepository.java
@@ -0,0 +1,7 @@
+package com.mabaya.demo.repositories;
+
+import com.mabaya.demo.model.Campaign;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CampaignRepository extends MongoRepository<Campaign,String> {
+}
--- src/main/java/com/mabaya/demo/services/CampaignService.java
+++ src/main/java/com/mabaya/demo/services/CampaignService.java
@@ -2,19 +2,28 @@ package com.mabaya.demo.services;
 
 import com.mabaya.demo.model.Campaign;
 import com.mabaya.demo.model.CampaignRequest;
+import com.mabaya.demo.model.CampaignState;
+import com.mabaya.demo.model.Product;
+import com.mabaya.demo.repositories.CampaignRepository;
 import com.mabaya.demo.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CampaignService {
 
     @Autowired
+    CampaignRepository campaignRepository;
+    @Autowired
     ProductRepository productRepository;
-
     public Campaign createCampaign(CampaignRequest request){
         //search all products and add in campaign
-
-        return new Campaign();
+        List<Product> products = productRepository.findBySellerId(request.getSellerId());
+        Campaign campaign = Campaign.builder().state(CampaignState.ACTIVE).bid(request.getBid()).name(request.getName())
+                .sellerId(request.getSellerId()).products(products).build();
+        campaignRepository.insert(campaign);
+        return campaign;
     };
 }
